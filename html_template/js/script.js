2ea2fb95b780d478e23d85de1a40a2d7804e0cec
CharleneGB <159122718+CharleneGB@users.noreply.github.com>
2024-02-13 20:31:04 +0100

    Update script.js

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -1,5 +1,18 @@
-let nom = prompt("👋 Bonjour, comment t'appelles-tu ?";
-let prenom = ` 👋Bonjour ${nom}`;
+let nom = prompt("👋 Bonjour, comment t'appelles-tu ?");
+let prenom = `👋Bonjour ${nom}`;
 
-document.body.innerHTML += `<h2>👋Bonjour ${nom}</h2>`
-              
+document.body.innerHTML += `<h2>👋Bonjour ${nom}</h2>`;
+
+let questionBirthYear = prompt("En quelle année es-tu née ?");
+let birthYear = (2024 - questionBirthYear);
+document.body.innerHTML += `<h3>Vous avez ${birthYear} ans </h3>`;
+
+const askName(nom) => {
+    let prenom = `👋Bonjour ${nom}`;
+    return document.body.innerHTML += `<h2>👋Bonjour ${nom}</h2>`;
+}
+
+const askBirthYear = (birthYear) =>{
+    let birthYear = (2024 - questionBirthYear);
+    return document.body.innerHTML += `<h3>Vous avez ${birthYear} ans </h3>`;
+}
