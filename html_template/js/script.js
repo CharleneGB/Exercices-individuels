7325864dffa38298e9e148b1b6a7abf2d57632c4
CharleneGB <159122718+CharleneGB@users.noreply.github.com>
2024-02-15 10:35:16 +0100

    Update script.js

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -16,3 +16,13 @@ const askBirthYear = (birthYear) =>{
     let birthYear = (2024 - questionBirthYear);
     return document.body.innerHTML += `<h3>Vous avez ${birthYear} ans </h3>`;
 }
+
+correction :
+
+function askAge(){
+    let birthYear = prompt("Entre ton année de naissance"); //ici je fais un prompt de l'année de naissance de la personne en lui demandant d'entrer son année de naissance
+    let today = new Date();
+    let year = today.getFullYear(); //ici je récupère l'année en cours
+    let age = year - birthYear;    
+    document.body.innerHTML += "<h3>Vous avez " + age + " ans</h3>";
+};
